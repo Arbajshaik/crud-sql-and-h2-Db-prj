2f494dd3e6763fc95bc021a3774a4836d90dcf72
arbaj <shaarbaj@in.ibm.com>
2020-09-29 17:50:16 +0530

    Changes of main file

---
+++ src/main/java/CrudSqlApplication.java
@@ -0,0 +1,21 @@
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+@EnableSwagger2
+@ComponentScan(basePackages="com.example.service,com.example.repository,com.example.controller")
+@EnableJpaRepositories(basePackages="com.example.repository")
+@EntityScan(basePackages="com.example.entity")
+@SpringBootApplication
+public class CrudSqlApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CrudSqlApplication.class, args);
+	}
+
+}
